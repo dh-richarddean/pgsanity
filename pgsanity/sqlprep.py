1b442a94663d090ab232b2e65efb7b62b4af7cc5
dh-richarddean <richard.dean@deliveryhero.com>
2019-02-05 13:45:55 +0100

    Some redshift syntax removals

--- pgsanity/sqlprep.py
+++ pgsanity/sqlprep.py
@@ -4,6 +4,21 @@ try:
 except ImportError:
     from io import StringIO
 
+CLEANUP_PATTERNS ={
+    'distkey': (r'(DISTKEY|((COMPOUND|INTERLEAVED)\s*)?SORTKEY)\s*\(.*\)', re.MULTILINE | re.IGNORECASE, ''),
+    'analyze': (r'PREDICATE\sCOLUMNS', re.IGNORECASE, ''),
+    'diststyle': (r'DISTSTYLE\s\w*', re.MULTILINE | re.IGNORECASE, ''),
+    'identity': (r'IDENTITY\s?\([0-9]*\,[0-9]*\s?\)', re.IGNORECASE, ''),
+    'window_ignore_nulls': (r'ignore\s*nulls\s*\)', re.IGNORECASE, ')')
+ }
+
+def _cleanup(input_str):
+    for pattern in CLEANUP_PATTERNS:
+        regex, regex_opts, sub_with = CLEANUP_PATTERNS[pattern]
+        regex_c = re.compile(regex, regex_opts)
+        input_str = re.sub(regex_c, sub_with, input_str)
+    return input_str
+
 def prepare_sql(sql, add_semicolon=False):
     results = StringIO()
 
@@ -13,6 +28,9 @@ def prepare_sql(sql, add_semicolon=False):
     for (start, end, contents) in split_sql(sql):
         precontents = None
         start_str = None
+
+        # Run our cleaning process
+        contents = _cleanup(contents)
 
         # decide where we are
         if not in_statement and not in_line_comment and not in_block_comment:
@@ -31,7 +49,8 @@ def prepare_sql(sql, add_semicolon=False):
 
         start_str = start_str or start or ""
         precontents = precontents or ""
-        results.write(start_str + precontents + contents)
+        if not (start_str + precontents + contents).isspace():
+            results.write(start_str + precontents + contents)
 
         if not in_line_comment and not in_block_comment and in_statement and end == ";":
             in_statement = False
